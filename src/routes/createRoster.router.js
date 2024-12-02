f4475b590072e7d750a06035571be1dc3155a1ad
ppiok-OwO <solcorin@naver.com>
2024-12-02 22:37:17 +0900

    ♻️ refactor 유효성 검사와 예외 처리, 그리고 리팩토링

--- src/routes/createRoster.router.js
+++ src/routes/createRoster.router.js
@@ -18,13 +18,28 @@ export async function teamPowerCheck(players) {
 /** 계정이 보유하고 있는 전체 선수 목록을 조회하는 API */
 router.get('/myPlayer', async (req, res, next) => {
   // TO-DO : 토큰 인증 미들웨어 추가
-
-  const { managerId } = req.body;
+  const { managerId } = req.body; // 이 부분을 토큰으로 받을 수 있을 것 같다.
 
   try {
+    // (1) 유효성 검사(1 이상의 정수인가?)
+    const isValidManagerId =
+      /^[0-9]+$/.test(+managerId) && Number(+managerId) >= 1;
+    // (2) 예외 처리
+    if (!managerId) {
+      return res.status(400).json({
+        error: 'managerId를 입력해 주세요.',
+      });
+    }
+    if (!isValidManagerId) {
+      return res.status(400).json({
+        error: 'managerId는 1이상의 정수여야 합니다.',
+      });
+    }
+
+    // 선수 목록 조회
     await prisma.$transaction(
       async (tx) => {
-        // 계정이 보유하고 있는 선수들의 id를 조회(각 요소가 객체 상태)
+        // 계정이 보유하고 있는 선수들의 id를 조회(이 배열의 요소는 객체 상태)
         const playersInTeam = await tx.teamtest.findMany({
           where: {
             managerId: +managerId,
@@ -34,9 +49,10 @@ router.get('/myPlayer', async (req, res, next) => {
           },
         });
 
-        // 요소의 밸류만 모아서 id들의 배열로 만든다.
+        // playersInTeam의 밸류에서 playerId값만 모아서 배열로 만든다.
         const playerIds = playersInTeam.map((player) => player.playerId);
 
+        // playerIds 배열을 where의 조건으로 설정한 뒤 선수 테이블을 조회. 이름이랑 스탯을 가져온다.
         const myPlayerList = await tx.playertest.findMany({
           where: {
             playerId: {
@@ -58,14 +74,33 @@ router.get('/myPlayer', async (req, res, next) => {
   }
 });
 
-/** 선수 목록에서 경기 출전 선수를 선택하는 API */
+/** 출전 선수를 선발하는 API */
 router.patch('/rosterIn', async (req, res, next) => {
   const { playerId1, playerId2, playerId3 } = req.body;
-  // const { playerId1, playerId2, playerId3 } = [1, 2, 3];
   // const { userEmail } = req.locals;
+  // TO-DO : 토큰 인증 미들웨어 추가
+
+  const playerIds = [playerId1, playerId2, playerId3];
+
+  // (1) 유효성 검사 함수
+  const isValidPlayerId = (playerId) =>
+    /^[0-9]+$/.test(+playerId) && Number(+playerId) >= 1;
+
+  // (2) 예외 처리
+  if (!playerId1 || !playerId2 || !playerId3) {
+    return res.status(400).json({
+      error: '세 개의 playerId를 모두 입력해주세요.',
+    });
+  }
+  const isValid = playerIds.every(isValidPlayerId);
+  if (!isValid) {
+    return res.status(400).json({
+      error: 'playerId는 1이상의 정수여야 합니다.',
+    });
+  }
 
   // 트랜젝션 내부에선 형변환이 제대로 되지 않는 경우가 있어서 미리 해준다.
-  const playerIdNumbers = [playerId1, playerId2, playerId3].map(Number);
+  const playerIdNumbers = playerIds.map(Number);
 
   try {
     const result = await prisma.$transaction(
@@ -82,7 +117,7 @@ router.patch('/rosterIn', async (req, res, next) => {
             isSelected: true,
           },
         });
-        // TO-DO : 트랜젝션 넣기
+
         const players = await tx.playertest.findMany({
           where: {
             // userEmail: +userEmail,
