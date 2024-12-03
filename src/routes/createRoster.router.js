3d4480dac90969cc438ea8d13a671cd459398289
ppiok-OwO <solcorin@naver.com>
2024-12-03 13:43:06 +0900

    ✨ update 출전 선수 변경 API

--- src/routes/createRoster.router.js
+++ src/routes/createRoster.router.js
@@ -15,6 +15,8 @@ const teamPowerCheck = (players) =>
 /** Number 형식 유효성 검사 함수(1이상의 정수를 받아야 할 때 사용) */
 const isValidInput = (input) => /^[0-9]+$/.test(+input) && Number(+input) >= 1;
 
+/** playerId 추출 함수 */
+
 /** 계정이 보유하고 있는 전체 선수 목록을 조회하는 API */
 router.get('/myPlayer', async (req, res, next) => {
   // TO-DO : 토큰 인증 미들웨어 추가
@@ -39,17 +41,17 @@ router.get('/myPlayer', async (req, res, next) => {
     await prisma.$transaction(
       async (tx) => {
         // 계정이 보유하고 있는 선수들의 id를 조회(이 배열의 요소는 객체 상태)
-        const playersInTeam = await tx.teamtest.findMany({
+        const membersInTeam = await tx.teamtest.findMany({
           where: {
             managerId: +managerId,
           },
           select: {
+            teamMemberId: true,
             playerId: true,
           },
         });
-
-        // playersInTeam의 밸류에서 playerId값만 모아서 배열로 만든다.
-        const playerIds = playersInTeam.map((player) => player.playerId);
+        // membersInTeam의 밸류에서 playerId값만 모아서 배열로 만든다.
+        const playerIds = membersInTeam.map((member) => member.playerId);
 
         // playerIds 배열을 where의 조건으로 설정한 뒤 선수 테이블을 조회. 이름이랑 스탯을 가져온다.
         const myPlayerList = await tx.playertest.findMany({
@@ -75,36 +77,36 @@ router.get('/myPlayer', async (req, res, next) => {
 
 /** 출전 선수를 선발하는 API */
 router.patch('/rosterIn', async (req, res, next) => {
-  const { playerId1, playerId2, playerId3 } = req.body;
+  const { teamMemberId1, teamMemberId2, teamMemberId3 } = req.body;
   // const { userEmail } = req.locals;
   // TO-DO : 토큰 인증 미들웨어 추가
 
-  const playerIds = [playerId1, playerId2, playerId3];
+  // 트랜젝션 내부에선 형변환이 제대로 되지 않는 경우가 있어서 미리 해준다.
+  const teamMemberIds = [teamMemberId1, teamMemberId2, teamMemberId3].map(
+    Number
+  );
 
   // (1) 유효성 검사(1 이상의 정수인가?)
-  const isValidPlayerId = playerIds.every(isValidInput);
+  const isValidteamMemberId = teamMemberIds.every(isValidInput);
 
   // (2) 예외 처리
-  if (!playerId1 || !playerId2 || !playerId3) {
+  if (!teamMemberId1 || !teamMemberId2 || !teamMemberId3) {
     return res.status(400).json({
-      error: '세 개의 playerId를 모두 입력해주세요.',
+      error: '세 개의 teamMemberId를 모두 입력해주세요.',
     });
   }
-  if (!isValidPlayerId) {
+  if (!isValidteamMemberId) {
     return res.status(400).json({
-      error: 'playerId는 1이상의 정수여야 합니다.',
+      error: 'teamMemberId는 1이상의 정수여야 합니다.',
     });
   }
-
-  // 트랜젝션 내부에선 형변환이 제대로 되지 않는 경우가 있어서 미리 해준다.
-  const playerIdNumbers = playerIds.map(Number);
 
   try {
     // id가 없는 선수를 선발했을 때(id에 1억을 입력한다거나...) 예외 처리
     const isValidPlayer = await prisma.teamtest.findMany({
       where: {
-        playerId: {
-          in: playerIdNumbers,
+        teamMemberId: {
+          in: teamMemberIds,
         },
       },
     });
@@ -115,12 +117,22 @@ router.patch('/rosterIn', async (req, res, next) => {
     // 선수들의 데이터를 배열로 가져오기
     const result = await prisma.$transaction(
       async (tx) => {
+        // 혹시! isSelected가 true인 선수가 이미 있다면 모두 false로 바꿔주기
+        await tx.teamtest.updateMany({
+          where: {
+            isSelected: true,
+          },
+          data: {
+            isSelected: false,
+          },
+        });
+
         // 선택한 선수의 isSelected 밸류를 true로 변경(게임이 끝나면 false로 바꿔주기)
         await tx.teamtest.updateMany({
           where: {
             // userEmail: +userEmail,
-            playerId: {
-              in: playerIdNumbers,
+            teamMemberId: {
+              in: teamMemberIds,
             },
           },
           data: {
@@ -128,11 +140,26 @@ router.patch('/rosterIn', async (req, res, next) => {
           },
         });
 
+        // 선발된 선수의 playerId 조회하기
+        const membersInRoster = await tx.teamtest.findMany({
+          where: {
+            teamMemberId: {
+              in: teamMemberIds,
+            },
+          },
+          select: {
+            teamMemberId: true,
+            playerId: true,
+          },
+        });
+        // membersInRoster의 밸류에서 playerId값만 모아서 배열로 만든다.
+        const playerIds = membersInRoster.map((member) => member.playerId);
+
         const players = await tx.playertest.findMany({
           where: {
             // userEmail: +userEmail,
             playerId: {
-              in: playerIdNumbers,
+              in: playerIds,
             },
           },
           select: {
@@ -154,6 +181,67 @@ router.patch('/rosterIn', async (req, res, next) => {
   }
 });
 
-// TO-DO : rosterOut
+/** 선발 선수를 다른 선수로 변경하는 API */
+router.patch('/rosterOut', async (req, res, next) => {
+  // TO-DO: 토큰 인증
+  // 교체할 선수 둘을 request body를 통해 요청받는다.
+  const { outMemberId, inMemberId } = req.body;
+  const memberIds = [outMemberId, inMemberId].map(Number);
+  try {
+    // 유효성 검사 (teamtest 테이블에서 조회했을 때 outPlayerId를 조회했을 때 isSelected가 true인가?)
+    await prisma.$transaction(
+      async (tx) => {
+        await tx.teamtest.updateMany({
+          where: {
+            teamMemberId: memberIds[0],
+          },
+          data: {
+            isSelected: false,
+          },
+        });
+        await tx.teamtest.updateMany({
+          where: {
+            teamMemberId: memberIds[1],
+          },
+          data: {
+            isSelected: true,
+          },
+        });
+      },
+      { isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted }
+    );
+
+    // 바뀐 선발 선수 명단을 반환
+    const membersInRoster = await prisma.teamtest.findMany({
+      where: {
+        isSelected: true,
+      },
+      select: {
+        teamMemberId: true,
+        playerId: true,
+      },
+    });
+    // membersInRoster의 밸류에서 playerId값만 모아서 배열로 만든다.
+    const playerIds = membersInRoster.map((member) => member.playerId);
+
+    const players = await prisma.playertest.findMany({
+      where: {
+        playerId: {
+          in: playerIds,
+        },
+      },
+      select: {
+        playerName: true,
+        playerStat: true,
+      },
+    });
+    // 예상 점수
+    const teamPower = await teamPowerCheck(players);
+
+    return res.status(201).json({ players, teamPower });
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
