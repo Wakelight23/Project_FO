34a3be4c8da4e136660d57ee5907b55c086b1740
wjdxo2412 <wjdxo2412@gmail.com>
2024-12-04 02:40:07 +0900

    ✨ update  : gacha 결과 와 팀멤버 생성 완료

--- src/routes/gacha.router.js
+++ src/routes/gacha.router.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
 const gachaRouter = express();
 
-const isLog = true;
+const isLog = false;
 
 const Log = (str) => {
   if (isLog) console.log(str);
@@ -107,7 +107,8 @@ const getRandomItem = async () => {
     Log('뽑기 시작');
     const items = await prisma.player.findMany();
     Log('뽑기 플레이어 가져옴');
-    console.log(items);
+    Log(items);
+
     const totalProbability = items.reduce((sum, item) => sum + item.rarity, 0);
     Log('뽑기의 가중치 모두 계산');
     const randomValue = Math.random() * totalProbability;
@@ -123,7 +124,7 @@ const getRandomItem = async () => {
       }
     }
   } catch (error) {
-    throw new Error('뽑기 함수 에러 팀원 김정태를 찌르세요!!' + error);
+    throw new Error('뽑기 함수 에러 팀원 김정태를 찌르세요!!');
   }
 };
 
@@ -143,16 +144,22 @@ gachaRouter.get('/api/gachas', async (req, res) => {
 //#region 단일 뽑기 정보
 //단일 뽑기 정보 조회
 gachaRouter.get('/api/gacha/:id', async (req, res) => {
+  const isAccess = true;
+
+  if (!isAccess) {
+    Log('잘못된 접근');
+  }
+
   try {
     const itemId = parseInt(req.params.id);
-    const item = items.find((i) => i.id === itemId);
-
+    const item = await prisma.player.findFirst({ where: { playerId: itemId } });
     if (item) {
       res.json({
         success: true,
         item: item,
       });
     } else {
+      Log('잘못된 아이템 ID 입니다.');
       res.status(404).json({
         success: false,
         message: '아이템을 찾을 수 없습니다.',
@@ -169,13 +176,28 @@ gachaRouter.get('/api/gacha/:id', async (req, res) => {
 //#region 뽑기 라우터
 //뽑기
 gachaRouter.post('/api/gacha', async (req, res) => {
-  const drawnItem = await getRandomItem();
-  Log(`결과 : ${drawnItem}`);
-  console.log(drawnItem);
-  res.json({
-    success: true,
-    item: { drawnItem },
-  });
+  try {
+    const { managerId } = req.body;
+    const drawnItem = await getRandomItem();
+    if (!drawnItem) {
+      return res.json({
+        success: false,
+        message: '500 고객센터에 문의하세요',
+      });
+    }
+    const teamMember = await prisma.teamMember.create({
+      data: {
+        playerId: drawnItem.playerId,
+        managerId: managerId,
+      },
+    });
+    return res.json({
+      success: true,
+      item: { drawnItem },
+    });
+  } catch (error) {
+    throw new Error('아이템 뽑기 라우터 에러 팀원 김정태를 찌르세요.' + error);
+  }
 });
 //#endregion
 
