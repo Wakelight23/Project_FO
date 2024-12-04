f6ef94c0842729c5e2f636321fc5336771c6f49e
wakelight23 <rlghks0223@gmail.com>
2024-12-04 17:33:15 +0900

    🩹 fix 데이터 베이스, 코드 수정
    - 변수명 수정
    - 데이터베이스 수정

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,96 +6,101 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default async function authM(req, res, next) {
-  try {
-    const { authorization } = req.headers;
-    console.log(authorization); // 추출 확인용
+    try {
+        const { authorization } = req.headers;
+        console.log(authorization); // 추출 확인용
 
-    // Authorization 헤더가 없을 경우
-    if (!authorization) throw new Error('토큰이 존재하지 않습니다.');
+        // Authorization 헤더가 없을 경우
+        if (!authorization) throw new Error('토큰이 존재하지 않습니다.');
 
-    const [tokenType, token] = authorization.split(' ');
+        const [tokenType, token] = authorization.split(' ');
 
-    if (tokenType !== 'Bearer')
-      throw new Error('토큰 타입이 일치하지 않습니다.');
+        if (tokenType !== 'Bearer')
+            throw new Error('토큰 타입이 일치하지 않습니다.');
 
-    // 엑세스 토큰 검증
-    const decodedToken = jwt.verify(token, process.env.SERVER_ACCESS_KEY);
-    const accountsData = decodedToken.accountid;
+        // 엑세스 토큰 검증
+        const decodedToken = jwt.verify(token, process.env.SERVER_ACCESS_KEY);
+        const accountsData = decodedToken.accountId;
 
-    // 데이터베이스에서 사용자 정보 조회
-    const accounts = await prisma.accounts.findFirst({
-      where: { accountid: accountsData },
-    });
-
-    // 사용자가 존재하지 않을 경우
-    if (!accounts) {
-      throw new Error('토큰 사용자가 존재하지 않습니다.');
-    }
-
-    // req.accounts 사용자 정보를 저장합니다.
-    req.account = accounts;
-    res.setHeader('Authorization', `Bearer ${decodedToken}`);
-    // console.log('인증된 정보 :', req.account); // 확인용
-    next();
-  } catch (error) {
-    if (error.name === 'TokenExpiredError') {
-      // 클라이언트가 전달한 엑세스 토큰이 만료된 경우
-      const { 'x-info': email } = req.headers;
-      const getaccountid = await prisma.accounts.findFirst({
-        where: { email },
-      });
-
-      try {
-        // 리프레시 토큰을 데이터베이스에서 조회
-        const storedToken = await prisma.refreshToken.findFirst({
-          where: { accountid: getaccountid.accountid },
+        // 데이터베이스에서 사용자 정보 조회
+        const accounts = await prisma.account.findFirst({
+            where: { accountId: accountsData },
         });
 
-        // 리프레시 토큰이 존재하지 않을 경우
-        if (!storedToken) {
-          return res.status(401).json({ message: '로그인이 필요합니다.' });
+        // 사용자가 존재하지 않을 경우
+        if (!accounts) {
+            throw new Error('토큰 사용자가 존재하지 않습니다.');
         }
 
-        // 리프레시 토큰 검증
-        const decodedRefreshToken = jwt.verify(
-          storedToken.token,
-          process.env.SERVER_REFRESH_KEY
-        );
-
-        // 검증된 리프레시토큰과 연결된 accountid를 바탕으로 새로운 엑세스 토큰 생성
-        const newAccessToken = jwt.sign(
-          { accountid: decodedRefreshToken.account_id },
-          process.env.SERVER_ACCESS_KEY,
-          { expiresIn: '1m' }
-        );
-
-        // 데이터베이스에서 계정 정보 조회
-        const newAccounts = await prisma.accounts.findFirst({
-          // 검증된 리프레시 토큰과 연계된 accounid로 계정정보 조회
-          where: { accountid: decodedRefreshToken.account_id },
-        });
-
-        //조회한 계졍정보 할당
-        req.account = newAccounts;
+        // req.accounts 사용자 정보를 저장합니다.
+        req.account = accounts;
+        res.setHeader('Authorization', `Bearer ${decodedToken}`);
         // console.log('인증된 정보 :', req.account); // 확인용
-        res.setHeader('Authorization', `Bearer ${newAccessToken}`);
         next();
-      } catch (refreshError) {
-        // 리프레시 토큰이 만료된 경우
-        if (refreshError.name === 'TokenExpiredError') {
-          return res.status(401).json({
-            message: '리프레시 토큰이 만료되었습니다. 다시 로그인하세요.',
-          });
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            // 클라이언트가 전달한 엑세스 토큰이 만료된 경우
+            const { 'x-info': email } = req.headers;
+            const getaccountId = await prisma.account.findFirst({
+                where: { email },
+            });
+
+            try {
+                // 리프레시 토큰을 데이터베이스에서 조회
+                const storedToken = await prisma.refreshToken.findFirst({
+                    where: { accountId: getaccountId.accountId },
+                });
+
+                // 리프레시 토큰이 존재하지 않을 경우
+                if (!storedToken) {
+                    return res
+                        .status(401)
+                        .json({ message: '로그인이 필요합니다.' });
+                }
+
+                // 리프레시 토큰 검증
+                const decodedRefreshToken = jwt.verify(
+                    storedToken.token,
+                    process.env.SERVER_REFRESH_KEY
+                );
+
+                // 검증된 리프레시토큰과 연결된 accountId를 바탕으로 새로운 엑세스 토큰 생성
+                const newAccessToken = jwt.sign(
+                    { accountId: decodedRefreshToken.account_id },
+                    process.env.SERVER_ACCESS_KEY,
+                    { expiresIn: '1m' }
+                );
+
+                // 데이터베이스에서 계정 정보 조회
+                const newAccounts = await prisma.account.findFirst({
+                    // 검증된 리프레시 토큰과 연계된 accounid로 계정정보 조회
+                    where: { accountId: decodedRefreshToken.account_id },
+                });
+
+                //조회한 계졍정보 할당
+                req.account = newAccounts;
+                // console.log('인증된 정보 :', req.account); // 확인용
+                res.setHeader('Authorization', `Bearer ${newAccessToken}`);
+                next();
+            } catch (refreshError) {
+                // 리프레시 토큰이 만료된 경우
+                if (refreshError.name === 'TokenExpiredError') {
+                    return res.status(401).json({
+                        message:
+                            '리프레시 토큰이 만료되었습니다. 다시 로그인하세요.',
+                    });
+                }
+                return res
+                    .status(401)
+                    .json({
+                        message: '리프레시 토큰 검증 중 오류가 발생했습니다.',
+                    });
+            }
+        } else if (error.name === 'JsonWebTokenError') {
+            // 예외
+            return res.status(401).json({ message: '토큰이 조작되었습니다.' });
+        } else {
+            return res.status(401).json({ message: '비정상적인 요청입니다.' });
         }
-        return res
-          .status(401)
-          .json({ message: '리프레시 토큰 검증 중 오류가 발생했습니다.' });
-      }
-    } else if (error.name === 'JsonWebTokenError') {
-      // 예외
-      return res.status(401).json({ message: '토큰이 조작되었습니다.' });
-    } else {
-      return res.status(401).json({ message: '비정상적인 요청입니다.' });
     }
-  }
 }
--- src/routes/sign_login/manager.router.js
+++ src/routes/sign_login/manager.router.js
@@ -8,48 +8,48 @@ dotenv.config();
 const router = express.Router();
 
 router.post('/create-manager', authM, async (req, res) => {
-  // 요청 본문에서 nickname 추출
-  const { nickname } = req.body;
-  // authM 미들웨어에서 인증을 거친 accounts 정보를 가져오고
-  // accounts에서 account_id를 추출한다
-  const { accountid } = req.account;
-  // const { name } = req.account;
-  // const { age } = req.account;
-  // const { email } = req.account;
-  // const { password } = req.account;
-  // console.log(accountid, name, age, email, password);
+    // 요청 본문에서 nickname 추출
+    const { nickname } = req.body;
+    // authM 미들웨어에서 인증을 거친 accounts 정보를 가져오고
+    // account에서 account_id를 추출한다
+    const { accountId } = req.account;
+    // const { name } = req.account;
+    // const { age } = req.account;
+    // const { email } = req.account;
+    // const { password } = req.account;
+    // console.log(accountId, name, age, email, password);
 
-  // 닉네임 중복 검증
-  const isExistnickname = await prisma.manager.findFirst({
-    where: { nickname },
-  });
+    // 닉네임 중복 검증
+    const isExistnickname = await prisma.manager.findFirst({
+        where: { nickname },
+    });
 
-  if (isExistnickname) {
-    return res.status(409).json({ message: '이미 존재하는 닉네임입니다.' });
-  }
+    if (isExistnickname) {
+        return res.status(409).json({ message: '이미 존재하는 닉네임입니다.' });
+    }
 
-  // 매니저 존재 검증
-  const isExistManager = await prisma.manager.findUnique({
-    where: { accountid: accountid },
-  });
+    // 매니저 존재 검증
+    const isExistManager = await prisma.manager.findUnique({
+        where: { accountId: accountId },
+    });
 
-  if (isExistManager) {
-    return res.status(409).json({ message: '이미 매니저가 존재합니다.' });
-  }
+    if (isExistManager) {
+        return res.status(409).json({ message: '이미 매니저가 존재합니다.' });
+    }
 
-  // 캐릭터 생성 로직
-  const newManager = await prisma.manager.create({
-    data: {
-      // 뽑아온 nickname, account_id를 각 컬럼에 적용한다.
-      accountid: accountid,
-      nickname: nickname,
-      rating: 1000,
-      cash: 10000,
-    },
-  });
-  return res
-    .status(201)
-    .json({ message: '매니저 생성 성공', manager: newManager });
+    // 캐릭터 생성 로직
+    const newManager = await prisma.manager.create({
+        data: {
+            // 뽑아온 nickname, account_id를 각 컬럼에 적용한다.
+            accountId: accountId,
+            nickname: nickname,
+            rating: 1000,
+            cash: 10000,
+        },
+    });
+    return res
+        .status(201)
+        .json({ message: '매니저 생성 성공', manager: newManager });
 });
 
 export default router;
