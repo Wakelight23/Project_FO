899be329ba122d210bfc92cbcb48230828a87609
wjdxo2412 <wjdxo2412@gmail.com>
2024-12-06 17:33:15 +0900

    ☄️ exception : 충돌 해결

--- src/routes/sign_login/delete.router.js
+++ src/routes/sign_login/delete.router.js
@@ -4,20 +4,6 @@ import authM from '../../middlewares/auth.js';
 
 const router = express.Router();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-router.delete('/delete/:managerId', authM, async (req, res) => {
-    const { accountId } = req.account;
-
-    // 삭제할 캐릭터의 ID 값을 가져옵니다.
-    const managerId = parseInt(req.params.managerId, 10);
-
-    // 삭제하려는 '캐릭터'을 가져옵니다. 없다면 에러를 발생시킵니다.
-    const manager = await prisma.manager.findUnique({
-        where: { managerId: managerId },
-=======
-=======
->>>>>>> ae9af60905f8ee1fe103c96701ebd2f0ed8c7ddb
 router.delete('/delete/:managerid', authM, async (req, res) => {
     const { accountid } = req.account;
 
@@ -27,10 +13,6 @@ router.delete('/delete/:managerid', authM, async (req, res) => {
     // 삭제하려는 '캐릭터'을 가져옵니다. 없다면 에러를 발생시킵니다.
     const manager = await prisma.manager.findUnique({
         where: { managerid: managerid },
-<<<<<<< HEAD
->>>>>>> 97df264409276b681807935068d3e9d30674e944
-=======
->>>>>>> ae9af60905f8ee1fe103c96701ebd2f0ed8c7ddb
     });
 
     if (!manager) {
@@ -40,22 +22,10 @@ router.delete('/delete/:managerid', authM, async (req, res) => {
     }
 
     // 조회된 캐릭터를 삭제합니다.
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // 매니저.accountId가 인증을 통해 현재 로그인한 사용자의 account.id와 같은지 판단
-    if (manager.accountId === accountId) {
-        await prisma.manager.delete({ where: { managerId } });
-=======
-    // 매니저.accountid가 인증을 통해 현재 로그인한 사용자의 account.id와 같은지 판단
-    if (manager.accountid === accountid) {
-        await prisma.manager.delete({ where: { managerid } });
->>>>>>> 97df264409276b681807935068d3e9d30674e944
-=======
-    // 매니저.accountid가 인증을 통해 현재 로그인한 사용자의 account.id와 같은지 판단
-    if (manager.accountid === accountid) {
-        await prisma.manager.delete({ where: { managerid } });
->>>>>>> ae9af60905f8ee1fe103c96701ebd2f0ed8c7ddb
 
+    // 매니저.accountid가 인증을 통해 현재 로그인한 사용자의 account.id와 같은지 판단
+    if (manager.accountid === accountid) {
+        await prisma.manager.delete({ where: { managerid } });
         return res.status(200).json({ message: '매니저가 삭제되었습니다.' });
     } else {
         return res
--- src/routes/sign_login/manager.router.js
+++ src/routes/sign_login/manager.router.js
@@ -10,8 +10,6 @@ const router = express.Router();
 router.post('/create-manager', authM, async (req, res) => {
     // 요청 본문에서 nickname 추출
     const { nickname } = req.body;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     // authM 미들웨어에서 인증을 거친 accounts 정보를 가져오고
     // accounts에서 account_id를 추출한다
@@ -36,42 +34,6 @@ router.post('/create-manager', authM, async (req, res) => {
         where: { accountId: +accountId },
     });
 
-=======
-=======
->>>>>>> ae9af60905f8ee1fe103c96701ebd2f0ed8c7ddb
-    // authM 미들웨어에서 인증을 거친 accounts 정보를 가져오고
-    // accounts에서 account_id를 추출한다
-    const { accountId } = req.account;
-    // const { name } = req.account;
-    // const { age } = req.account;
-    // const { email } = req.account;
-    // const { password } = req.account;
-    // console.log(accountid, name, age, email, password);
-
-    // 닉네임 중복 검증
-    const isExistnickname = await prisma.manager.findFirst({
-        where: { nickname },
-    });
-
-    if (isExistnickname) {
-        return res.status(409).json({ message: '이미 존재하는 닉네임입니다.' });
-    }
-
-    if (!/^.{1,8}$/.test(nickname)) {
-        return res
-            .status(409)
-            .json({ message: 'nickname은 8자리 이하로만 설정할 수 있습니다' });
-    }
-
-    // 매니저 존재 검증
-    const isExistManager = await prisma.manager.findUnique({
-        where: { accountId: +accountId },
-    });
-
-<<<<<<< HEAD
->>>>>>> 97df264409276b681807935068d3e9d30674e944
-=======
->>>>>>> ae9af60905f8ee1fe103c96701ebd2f0ed8c7ddb
     if (isExistManager) {
         return res.status(409).json({ message: '이미 매니저가 존재합니다.' });
     }
@@ -81,13 +43,6 @@ router.post('/create-manager', authM, async (req, res) => {
         data: {
             // 뽑아온 nickname, account_id를 각 컬럼에 적용한다.
             accountId: +accountId,
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 97df264409276b681807935068d3e9d30674e944
-=======
->>>>>>> ae9af60905f8ee1fe103c96701ebd2f0ed8c7ddb
             nickname: nickname,
             rating: 1000,
             cash: 10000,
